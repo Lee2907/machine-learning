877c50cf617b115febb214060f0502659584b706
Lee2907 <kamvelihle.buka2907@gmail.com>
2021-06-01 12:57:19 +0200

    commit

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ import numpy as np
 
 # Original array
 array = np.arange(0,21)
-print(array)
+
 
 r1 = np.mean(array)
-
-
 r2 = np.std(array)
-
-
 r3 = np.var(array)
 
+print(array)
 print("Mean: ", r1)
 print("Std: ", r2)
 print("Variance: ", r3)
