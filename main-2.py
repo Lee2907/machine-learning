a707e1d364b551645c52fb0fe645e986a7917530
Lee2907 <kamvelihle.buka2907@gmail.com>
2021-06-02 09:09:35 +0200

    commit

--- main-2.py
+++ main-2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+# Challenge 2
 nums = [0.5,0.7,1,1.2,1.3,2.1]
 bins = [0,1,2,3]
 
@@ -8,3 +9,7 @@ plt.ylabel("Bins")
 plt.title("Histogram of nums against bins")
 plt.hist(nums,bins)
 plt.show()
+
+tuple1=(1,2,4,3)
+x=tuple1[1:3]
+print (x)
